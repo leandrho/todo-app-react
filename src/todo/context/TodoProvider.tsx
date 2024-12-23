710369e78ee0aa7ff4bfc910dd5ec612c99a59fe
Leandrho <leandro.coder01@gmail.com>
2024-12-23 10:58:12 -0300

    ...

--- src/todo/context/TodoProvider.tsx
+++ src/todo/context/TodoProvider.tsx
@@ -1,13 +1,21 @@
 
+import { useReducer } from "react"
 import { TodoContext } from "./TodoContext"
+import { InitialState, todoReducer } from "./todoReducer"
 
 type TodoProviderProps = {
     children :JSX.Element | JSX.Element[]
 }
 
 export const TodoProvider = ({children}:TodoProviderProps) => {
+  
+  const [state, dispatch] = useReducer(todoReducer, InitialState);
+  
   return (
-        <TodoContext.Provider value={{}}>
+        <TodoContext.Provider value={{
+            state,
+            dispatch
+        }}>
             {children}
         </TodoContext.Provider>
   )
--- src/todo/context/todoReducer.ts
+++ src/todo/context/todoReducer.ts
@@ -1,18 +1,44 @@
 import type { Todo, TodoState } from "../interfaces/interfaces";
 
-type TodoAction = 
+export type TodoAction = 
         {type: 'add-todo', payload: Todo} |
         {type: 'toggle-todo', payload: {id: string}} 
 
-export const todoReducer = (state :TodoState, action: TodoAction ) : TodoState =>{
+// export const InitialState : TodoState = {
+//     todoCount: 0,
+//     todos: [],
+//     completed: 0,
+//     pending: 0
+// }
+export const InitialState : TodoState ={
+    todoCount: 2,
+    todos: [
+        {
+            id: '1',
+            desc:  "Recolectar las piedras",
+            completed: false
+        },
+        {
+            id: '2',
+            desc:  "Recolectar las aguas",
+            completed: false
+        }
+    ],
+    completed: 0,
+    pending: 0
+}
+export const todoReducer = (state :TodoState = InitialState, action: TodoAction ) : TodoState =>{
 
     switch (action.type) {
         case "add-todo":
-            
+            console.log('add todo')
             return {
                 ...state,
                 todos: [...state.todos, action.payload]
             }
+        case "toggle-todo":
+            console.log('toggle todo')
+            return state;
         default:
             return state;
     }

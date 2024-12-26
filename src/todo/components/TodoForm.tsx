5edddac9f85f53a27a848137775ecba31e0f3dc1
Leandrho <leandro.coder01@gmail.com>
2024-12-26 16:53:56 -0300

    ...

--- src/todo/components/TodoForm.tsx
+++ src/todo/components/TodoForm.tsx
@@ -2,7 +2,7 @@
 
 export const TodoForm = () => {
   return (
-    <form action="" className="flex gap-1 p-8 bg-red-100 max-w-fit">
+    <form action="" className="flex gap-1 p-8 bg-red-100 max-w-fit mb-4">
         <div className="flex flex-col gap-2 flex-grow ">
           <label htmlFor="todo" className="text-red-800">Nueva tarea</label>
           <input id='todo' type="text" className="min-w-80 border border-red-200 rounded-lg min-h-10 p-2 outline-none"></input>

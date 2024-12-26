369a631dcb9f387d1a8753de4cb3f67278e989d7
Leandrho <leandro.coder01@gmail.com>
2024-12-26 16:18:29 -0300

    start with todoform component

---
+++ src/todo/components/TodoForm.tsx
@@ -0,0 +1,11 @@
+
+
+export const TodoForm = () => {
+  return (
+    <form action="">
+        <label htmlFor="todo">Tarea</label>
+        <input id='todo' type="text"></input>
+        <button type="submit">Agregar</button>
+    </form>
+  )
+}

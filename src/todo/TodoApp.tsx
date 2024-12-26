14623f6c0199948b94f00e9d8a2f25f471176731
Leandrho <leandro.coder01@gmail.com>
2024-12-26 16:46:58 -0300

    add todoform to todoapp

--- src/todo/TodoApp.tsx
+++ src/todo/TodoApp.tsx
@@ -1,4 +1,5 @@
 
+import { TodoForm } from './components/TodoForm';
 import { useTodo } from "./hooks/useTodo"
 import type { Todo } from "./interfaces/interfaces"
 
@@ -7,7 +8,7 @@ export const TodoApp = () => {
     const {todos} = useTodo();
   return (
     <>
-
+            <TodoForm />
             <h1 className="text-red-300 text-6xl">Todos:</h1>
             {
                 todos.map((todo: Todo) => (

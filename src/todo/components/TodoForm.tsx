10445a76c08f7c473ceda339be058772a14c6f29
Leandrho <leandro.coder01@gmail.com>
2024-12-26 16:46:22 -0300

    styling form

--- src/todo/components/TodoForm.tsx
+++ src/todo/components/TodoForm.tsx
@@ -2,10 +2,12 @@
 
 export const TodoForm = () => {
   return (
-    <form action="">
-        <label htmlFor="todo">Tarea</label>
-        <input id='todo' type="text"></input>
-        <button type="submit">Agregar</button>
+    <form action="" className="flex gap-1 p-8 bg-red-100 max-w-fit">
+        <div className="flex flex-col gap-2 flex-grow ">
+          <label htmlFor="todo" className="text-red-800">Nueva tarea</label>
+          <input id='todo' type="text" className="min-w-80 border border-red-200 rounded-lg min-h-10 p-2 outline-none"></input>
+        </div>
+        <button type="submit" className="min-w-24 bg-slate-600 rounded-lg text-red-300 font-bold">Agregar</button>
     </form>
   )
 }

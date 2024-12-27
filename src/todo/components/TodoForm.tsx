ef7a9cf94197a9bca568ba311b092e2ff0d7b990
Leandrho <leandro.coder01@gmail.com>
2024-12-27 13:23:57 -0300

    ...

--- src/todo/components/TodoForm.tsx
+++ src/todo/components/TodoForm.tsx
@@ -24,7 +24,7 @@ export const TodoForm = () => {
   return (
     <form action="" className="flex gap-4 p-8 bg-red-100 max-w-fit rounded-lg mb-4 shadow-sm" onSubmit={(e)=>handleSubmit(e)}>
         <div className="flex flex-col gap-2 flex-grow ">
-          <label htmlFor="todo" className="text-slate-600 font-semibold">Nueva tarea</label>
+          <label htmlFor="todo" className="text-slate-800 font-semibold">Nueva tarea</label>
           <input ref={todoInput} id='todo' type="text" className="min-w-80 border border-red-200 rounded-lg min-h-10 p-2 outline-none"></input>
         </div>
         <button type="submit" className="text-[15px] min-w-24 bg-slate-600 rounded-lg text-red-300 font-bold hover:bg-slate-500">Agregar</button>
